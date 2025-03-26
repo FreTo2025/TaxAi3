25343b0cd233a72f9134b29cc1202cfbe5a5ebb6
FreTo2025 <ftcasesorias@gmail.com>
2025-03-26 16:54:18 -0500

    Update googleSheetsService.js

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -1,6 +1,61 @@
 import path from 'path';
 import { google } from 'googleapis';
 
+import fs from 'fs';
+const credentialsPath = path.join(process.cwd(), 'src/credentials', 'credentials.json');
+const content = fs.readFileSync(credentialsPath, 'utf-8');
+console.log('📄 Primeras líneas de private_key:\n', JSON.parse(content).private_key.split('\n').slice(0, 3));
+
+
+
+const sheets = google.sheets('v4');
+
+async function addRowToSheet(auth, spreadsheetId, values) {
+    const request = {
+        spreadsheetId,
+        range: 'reservas',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: [values],
+        },
+        auth,
+    }
+
+    try {
+        const response = (await sheets.spreadsheets.values.append(request).data);
+        return response;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const appendToSheet = async (data) => {
+    try {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: path.join(process.cwd(), 'src/credentials', 'credentials.json'),
+            scopes: ['https://www.googleapis.com/auth/spreadsheets']
+        });
+
+        const authClient = await auth.getClient();
+        const spreadsheetId = '1cUyad9EmmRlvbOWyecWTLHJSvmILy0WLopPpvHYWMjg'
+
+
+auth.getClient().then(() => {
+  console.log('✔️ Autenticación exitosa');
+}).catch(err => {
+  console.error('❌ Error autenticando:', err);
+});
+
+
+
+        
+        await addRowToSheet(authClient, spreadsheetId, data);
+        return 'Datos correctamente agregados'
+    } catch (error) {
+        console.error(error);
+    }
+}
 async function testAuth() {
   try {
     const auth = new google.auth.GoogleAuth({
@@ -16,5 +71,4 @@ async function testAuth() {
   }
 }
 
-testAuth();
-
+export default appendToSheet, testAuth;
