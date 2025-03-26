2304fed08b05ba68949f6c6ea6b170fb31ceca8c
FreTo2025 <ftcasesorias@gmail.com>
2025-03-26 17:00:18 -0500

    Update googleSheetsService.js

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -56,19 +56,5 @@ auth.getClient().then(() => {
         console.error(error);
     }
 }
-async function testAuth() {
-  try {
-    const auth = new google.auth.GoogleAuth({
-      keyFile: path.join(process.cwd(), 'src/credentials', 'credentials.json'),
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
 
-    const client = await auth.getClient();
-    const token = await client.getAccessToken();
-    console.log('✅ Token obtenido:', token);
-  } catch (err) {
-    console.error('❌ Error en autenticación:', err.response?.data || err.message || err);
-  }
-}
-
-export default appendToSheet, testAuth;
+export default appendToSheet;
