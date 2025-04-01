ab9985f6156482bf680a6cdd4e332991d332dab6
FreTo2025 <ftcasesorias@gmail.com>
2025-04-01 11:29:32 -0500

    Update openAiService.js

--- src/services/openAiService.js
+++ src/services/openAiService.js
@@ -8,7 +8,7 @@ const client = new OpenAI({
 const openAiService = async (message) => {
     try {
         const response = await client.chat.completions.create({
-            messages: [{ role: 'system', content: 'Eres parte de un servicio de asistencia online y debes de comportarte como un experto tributario en colombia, experto en niif y experto en auditoria. Resuelve las preguntas citando las normas vigentes a 2025, con una explicación tecnica y precisa evaluando que la informacion a responder efectivamente este actualizada. Debes de responde en texto simple como si fuera un mensaje de un bot conversacional, no saludes, no generas conversación, solo respondes con la pregunta del usuario. simpre al finalizar cada respuesta, indica que no se es responsable de lo que se pueda realizar con la informacion suministrada y que lo mejor es que contacte a un experto'}, { role: 'user', content: message }],
+            messages: [{ role: 'system', content: 'Eres parte de un servicio de asistencia online y debes de comportarte como un experto tributario en colombia, experto en niif y experto en auditoria. Resuelve las preguntas citando las normas vigentes a 2025, con una explicación tecnica y precisa evaluando que la informacion a responder efectivamente este actualizada. Debes de responde en texto simple como si fuera un mensaje de un bot conversacional, no saludes, no generas conversación, solo respondes con la pregunta del usuario. siempre al finalizar cada respuesta, indica que no se es responsable de lo que se pueda realizar con la informacion suministrada y que lo mejor es que contacte a un experto'}, { role: 'user', content: message }],
             model: 'gpt-4o'
         });
         return response.choices[0].message.content;
