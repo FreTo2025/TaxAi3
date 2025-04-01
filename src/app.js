e93911b05ea8fc8e2a414768f58f9497baae2693
FreTo2025 <ftcasesorias@gmail.com>
2025-04-01 11:25:55 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import express from 'express';
 import config from './config/env.js';
 import webhookRoutes from './routes/webhookRoutes.js';
-import { testGoogleAuth } from './services/authCheck.js';
-
-testGoogleAuth();
-
 
 const app = express();
 app.use(express.json());
